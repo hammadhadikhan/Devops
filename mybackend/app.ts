0c44a86cf99e32dac0e9ba42c8a5f8bf343d99ea
hammadhadikhan <124785647+hammadhadikhan@users.noreply.github.com>
2024-06-09 00:40:51 +0500

    Add files via upload

---
+++ mybackend/app.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import coursesRouter from './routes/courses-routes';
+
+//check CI/CD
+const PORT = 5000;
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4000', // Update this to match your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify the allowed headers
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/courses', coursesRouter)
+
+app.listen(PORT, () => {
+  console.log('App started at ', PORT)
+})
---
+++ mybackend/routes/courses-routes.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import { prisma } from '../prisma/db';
+const router = express.Router();
+//added comment
+
+router.get('/', async (req, res) => {
+    const courses = await prisma.course.findMany();
+    return res.json({ courses }).status(200)
+})
+
+
+router.post('/', async (req, res) => {
+    const body = req.body;
+    console.log({ body })
+    const course = await prisma.course.create({
+        data: {
+            title: body.title,
+            description: body.description,
+            rating: body.rating
+        }
+    })
+    return res.json({ course }).status(201)
+})
+
+
+export default router;
